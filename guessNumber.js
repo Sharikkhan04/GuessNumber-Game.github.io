33985168395560a38c62033028bbaf61962eded0
Sharikkhan04 <100661235+Sharikkhan04@users.noreply.github.com>
2023-04-25 23:21:17 +0530

    commit

--- guessNumber.js
+++ guessNumber.js
@@ -1,15 +1,25 @@
 let Score = document.getElementById("score");
+let Btn = document.getElementById("btn")
+let Btn2 = document.getElementById("btn2")
 let showResult = document.getElementById("result");
 let showAttempt = document.getElementById("attempt");
 let input = document.querySelector("#userinput");
 let chance = 0;
 let randNum = Math.floor(Math.random() * 100) + 1;
+console.log(randNum);
+function reset(){
+   location.reload();
+   input.value = "";
+   document.getElementsByName('text')[0].placeholder = 'Enter your guess...';
+}
 function submit() {
    let choice = input.value;
    if (randNum == choice) {
       showResult.style.color = "green"
       showResult.innerText = "congrats !  you got the right num";
       Score.innerText += 100 - chance;
+      Btn.style.display="none";
+      Btn2.style.display="block";
    }
    else if (choice < randNum) {
       showResult.innerText = "the value is greater than your choice";
